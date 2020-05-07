4cc353734ad8a5936bb1f992700ea0e693e25138
Lukas Jungmann <lukas.jungmann@oracle.com>
2020-05-07 23:14:02 +0200

    integrate m-api 3.2.3
    Signed-off-by: Lukas Jungmann <lukas.jungmann@oracle.com>

--- impl/src/main/java/module-info.java
+++ impl/src/main/java/module-info.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2019, 2020 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Distribution License v. 1.0, which is available at
@@ -10,7 +10,7 @@
 
 module gmbal {
     requires java.logging;
-    requires transitive java.management;
+    requires transitive org.glassfish.external.management.api;
 
     exports org.glassfish.gmbal;
     exports org.glassfish.gmbal.impl.trace;
